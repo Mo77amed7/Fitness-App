bee49b219f35ca51e8f1650a400af1ac10be5ba7
Mo77amed7 <muhamedrashad99@gmail.com>
2025-12-16 22:44:07 +0200

    Refactor code structure for improved readability and maintainability

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
-<template></template>
-<script></script>
+<template>
+  <layout> </layout>
+</template>
+<script setup>
+import layout from "@/components/layouts/Layout.vue";
+</script>
 <style scoped></style>
---
+++ src/components/layouts/Layout.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1>SMOLGRAM</h1>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+  <footer>
+    <p>Created By</p>
+    <a href="https://github.com/Mo77amed7" target="_blank">
+      <img
+        src="https://avatars.githubusercontent.com/u/99067002?v=4"
+        alt="Profile"
+      />
+      <p>Mohamed</p>
+      <i class="fa-brands fa-github"></i>
+    </a>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+a {
+  text-decoration: none;
+}
+a {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  border: 1px solid transparent;
+  border-radius: 6px;
+  padding: 2px;
+}
+a:hover {
+  border-color: #2563eb;
+}
+img {
+  width: 25px;
+  border-radius: 50%;
+}
+</style>
