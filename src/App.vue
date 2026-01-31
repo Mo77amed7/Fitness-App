17dbedd48a65a8a419dc01717c3f38193555eada
Mo77amed7 <muhamedrashad99@gmail.com>
2026-01-31 23:51:21 +0200

    feat: update Dashboard component and restructure Workout page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <layout>
-    <Welcome v-if="ablePage == 1"/>
-    <Workout v-if="ablePage == 2"/>
+    <Welcome v-if="ablePage == 1" />
+    <Dashboard v-if="ablePage == 2" />
+    <Workout v-if="ablePage == 3" />
   </layout>
 </template>
 <script setup>
 import Layout from "@/components/layouts/Layout.vue";
 import Welcome from "@/components/pages/Welcome.vue";
 import Workout from "@/components/pages/Workout.vue";
-const ablePage = 2;
+import Dashboard from "./components/pages/Dashboard.vue";
+const ablePage = 3;
 </script>
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/pages/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <div>
+      <h2>Welcome Smoldier</h2>
+      <div class="tip">
+        <h3>Daily Tip</h3>
+        <p>{{ tip }}</p>
+      </div>
+      <a href="">Start Workout</a>
+    </div>
+  </section>
+  <section>
+    <Grid />
+  </section>
+</template>
+
+<script setup>
+import { gymHealthFacts } from "@/utils/index";
+import Grid from "@/components/Grid.vue";
+const tip = gymHealthFacts[Math.floor(Math.random() * gymHealthFacts.length)];
+</script>
+
+<style scoped>
+section {
+  padding: 1rem;
+}
+.tip {
+  background-color: #f3f4f6;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  margin: 1rem 0;
+}
+a {
+  display: inline-block;
+  background-color: #26539b;
+  color: white;
+  padding: 0.75rem 1.5rem;
+  border-radius: 0.5rem;
+  text-decoration: none;
+  font-weight: bold;
+}
+a:hover {
+  background-color: #2563eb;
+}
+</style>
